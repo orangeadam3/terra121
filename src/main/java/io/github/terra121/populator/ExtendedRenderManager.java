bc3272139597687d304ebeed5cf91ff91007cd25
taeko-chan <felix.cordone@gmail.com>
2020-04-15 22:55:18 +0200

    modified ExtendedRenderer for better performance, still crashes tho

---
+++ src/main/java/io/github/terra121/populator/ExtendedRenderManager.java
@@ -0,0 +1,15 @@
+package io.github.terra121.populator;
+
+import io.github.opencubicchunks.cubicchunks.api.util.CubePos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Doesn't do very much at the moment, asside from storing the cache of the covered area of one pregen
+ * */
+public class ExtendedRenderManager {
+
+    public static List<CubePos> covered = new ArrayList<>();
+
+}
