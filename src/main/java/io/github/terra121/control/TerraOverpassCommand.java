641e2ee5374225e608ff5fd5b3804069fbc39e6a
SmylerMC <smyler@mail.com>
2020-07-12 19:53:20 +0200

    en_us localization for overpass command

--- src/main/java/io/github/terra121/control/TerraOverpassCommand.java
+++ src/main/java/io/github/terra121/control/TerraOverpassCommand.java
@@ -2,6 +2,7 @@ package io.github.terra121.control;
 
 import io.github.terra121.TerraConfig;
 import io.github.terra121.dataset.OpenStreetMaps;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -9,6 +10,9 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 
+//TODO Tab completion
+//TODO Permissions
+//TODO Cancel tread
 public class TerraOverpassCommand extends CommandBase {
 
 	public static final String COMMAND_NAME = "overpass";
@@ -20,42 +24,42 @@ public class TerraOverpassCommand extends CommandBase {
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		return COMMAND_NAME + " <status|list|default|fallback>";
+		return "terra121.commands.overpass.usage";
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if(args.length != 1) throw new CommandException("Invalid number of arguments"); //TODO Localize
+		if(args.length != 1) throw new CommandException("terra121.commands.overpass.usage");
 		String inUse = OpenStreetMaps.getOverpassEndpoint();
 		boolean isDefault = inUse.equals(TerraConfig.serverOverpassDefault);
 		boolean hasFallback = !TerraConfig.serverOverpassFallback.equals("");
 		switch(args[0]) {
 		case "status":
-			String t = isDefault? "default": "fallback";
-			ITextComponent msg = new TextComponentString("Currently using " + t + " Overpass endpoint: " + inUse); //TODO Localize
+			String t = isDefault? "terra121.commands.overpass.status.default": "terra121.commands.overpass.status.fallback";
+			ITextComponent msg = new TextComponentString(I18n.format(t, inUse));
 			sender.sendMessage(msg);
 			break;
 		case "list":
-			ITextComponent msg1 = new TextComponentString("Default:  " + TerraConfig.serverOverpassDefault); //TODO Localize
+			ITextComponent msg1 = new TextComponentString(I18n.format("terra121.commands.overpass.list.default", TerraConfig.serverOverpassDefault));
 			ITextComponent msg2 = new TextComponentString(hasFallback?
-					"Fallback: " + TerraConfig.serverOverpassFallback:
-					"No fallback endpoint is set in the config"); //TODO Localize
+					I18n.format("terra121.commands.overpass.list.fallback", TerraConfig.serverOverpassFallback):
+					I18n.format("terra121.commands.overpass.list.nofallback"));
 			sender.sendMessage(msg1);
 			sender.sendMessage(msg2);
 			break;
 		case "default":
 			OpenStreetMaps.setOverpassEndpoint(TerraConfig.serverOverpassDefault);
-			msg = new TextComponentString("Now using the default endpoint"); //TODO Localize
+			msg = new TextComponentString(I18n.format("terra121.commands.overpass.set.default"));
 			sender.sendMessage(msg);
-			break; //TODO main
+			break;
 		case "fallback":
-			if(!hasFallback) throw new CommandException("No fallback endpoint is set in the config"); //TODO Localize
+			if(!hasFallback) throw new CommandException(I18n.format("terra121.commands.overpass.list.nofallback"));
 			OpenStreetMaps.setOverpassEndpoint(TerraConfig.serverOverpassFallback);
-			msg = new TextComponentString("Now using the fallback endpoint"); //TODO Localize
+			msg = new TextComponentString(I18n.format("terra121.commands.overpass.set.fallback"));
 			sender.sendMessage(msg);
-			break; //TODO fallback
+			break;
 		default:
-			throw new CommandException("Invalid argument"); //TODO Localize
+			throw new CommandException("terra121.commands.overpass.usage");
 		}
 	}
 
